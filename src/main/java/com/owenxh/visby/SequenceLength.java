bf6bd54b72539a29ad670e552c2e5688443d4447
Owenxh <sowen1023@gmail.com>
2018-05-12 22:04:55 +0800

    Add SequenceLength

---
+++ src/main/java/com/owenxh/visby/SequenceLength.java
@@ -0,0 +1,15 @@
+package com.owenxh.visby;
+
+/**
+ * 序列号长度
+ *
+ * @author <a href="mailto:sowen1023@gmail.com">Owen.Yuan</a>
+ * @since 2018/5/12
+ */
+public interface SequenceLength {
+
+    /**
+     * 序列号长度
+     */
+    int length();
+}
